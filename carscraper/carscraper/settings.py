da8aac15ed193d65ad5a35af0333d5d8f33f9990
Anshal Chopra <anshalchopra@d192-075-242-225.wireless.sfu.ca>
2024-01-29 15:30:56 -0800

    Active Links of Cars Available in Canada

--- carscraper/carscraper/settings.py
+++ carscraper/carscraper/settings.py
@@ -15,12 +15,11 @@ NEWSPIDER_MODULE = "carscraper.spiders"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.01"
-
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 64
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
--- carscraper/carscraper/spiders/carspider.py
+++ carscraper/carscraper/spiders/carspider.py
@@ -1,6 +1,8 @@
 import scrapy
 import json
 import os
+from bs4 import BeautifulSoup
+import requests
 
 
 class CarspiderSpider(scrapy.Spider):
@@ -9,42 +11,52 @@ class CarspiderSpider(scrapy.Spider):
     start_urls = ["https://www.autotrader.ca"]
 
     def parse(self, response):
-        yield from self.get_cars(response)
+        self.get_cars(response)
+
         with open('Brands.json', 'r') as f:
             data = json.load(f)
         
-        for item in data:
-            brand = item["Brand"]
-            link = item["URL"]
-            yield scrapy.Request(url = link, callback = self.extract_car_links, meta = {'brand': brand})
-
-
-
-
+        for brand, info in data.items():
+            brand = info["Brand"]
+            link = info["URL"]
+            yield scrapy.Request(url = link, callback = self.extract_car_links, meta = {'brand': brand, 'counter': 0}, dont_filter=True)
 
     def get_cars(self, response):
         cars = response.css('option::text').getall()[1:-1]
+        car_dict = {}
         for car in cars:
             car = car.strip()
             link = car.replace(" ", "%20")
-            yield{
+            car_dict[car] = {
                 "Brand": car,
-                "URL": "https://www.autotrader.ca/cars/" + link + "/?rcp=15&rcs=0&srt=35&prx=-1&loc=British%20Columbia&hprc=True&wcp=True&inMarket=advancedSearch"
+                "URL": "https://www.autotrader.ca/cars/" + link + "?rcp=100&rcs=0&srt=35&prx=-1&loc=Toronto%2C%20ON&hprc=True&wcp=True&inMarket=advancedSearch"
             }
-    
+        if os.path.exists('Brands.json'):
+            os.remove('Brands.json')
+        with open('Brands.json', 'w') as f:
+            json.dump(car_dict, f)
+
+
     def extract_car_links(self, response):
-        counter = 0
-        urls = response.css("a.inner-link::attr('href')").get_all()
-        cars = response.css("spam.title-with-trim::text").get_all()
+        counter = response.meta['counter']
+        num_cars = response.meta.get('num_cars')
+        num_pages = response.meta.get('num_pages')
+        if counter == 0:
+            num_cars = response.css('span[id="titleCount"]::text').get()
+            num_cars = num_cars.replace(",","")
+            num_pages = (int(num_cars.strip())//100) * 100
+        print(num_cars)
+
+        urls = response.css("a.inner-link::attr('href')").getall()
+        cars = response.css("span.title-with-trim::text").getall()
         for car, url in zip(cars, urls):
             result = {
                 "Car-Title": car.strip(),
                 "Active-URL": url
             }
-
             filename = response.meta['brand'] + '.json'
             if not os.path.exists(filename):
-                with open(filename, 'w'):
+                with open(filename, 'w') as f:
                     json.dump([result], f)
             else:
                 with open(filename, 'r+') as f:
@@ -52,7 +64,8 @@ class CarspiderSpider(scrapy.Spider):
                     data.append(result)
                     f.seek(0)
                     json.dump(data, f)
-        self.counter += 15
-        next_link = response.url.replace("rcs="+str(self.counter - 15), "rcs="+str(self.counter))
-
-
+        next_link = response.url
+        while(counter < num_pages):
+            next_link = response.url.replace("rcs="+str(counter), "rcs="+str(counter + 100))
+            counter = counter + 100
+            yield response.follow(next_link, callback = self.extract_car_links, meta = {'brand': response.meta['brand'], 'counter': counter, 'num_cars': num_cars, 'num_pages': num_pages})
